d4a35cffaa22e5f2d19ac5ba2dec06c58e4b6499
Isaiah Magana <Isaiah2Mag@gmail.com>
2024-02-06 00:39:17 -0500

    Show an example of single line comments

--- src/main/java/com/maganadev/javacomments/JavaComments.java
+++ src/main/java/com/maganadev/javacomments/JavaComments.java
@@ -1,16 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.maganadev.javacomments;
-
 /**
  *
  * @author isaiahm
  */
 public class JavaComments {
-
+    /**
+     * Main method to run program
+     */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome Java!"); //Welcome message for java
+        System.out.println("You were born in 1995");
+        //Java's release was announced by Sun Microsystems in 1995
+        
     }
 }

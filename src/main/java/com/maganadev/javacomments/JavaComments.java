1508531b72ccedc7a1bf9d647078873b274cc8cc
Isaiah Magana <Isaiah2Mag@gmail.com>
2024-02-06 00:34:29 -0500

    Create initial project file and directories

---
+++ src/main/java/com/maganadev/javacomments/JavaComments.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.maganadev.javacomments;
+
+/**
+ *
+ * @author isaiahm
+ */
+public class JavaComments {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

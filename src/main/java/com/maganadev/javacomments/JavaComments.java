3816ec7a8a4cc2b990264764f4f7a19e7ec3bb92
Isaiah Magana <Isaiah2Mag@gmail.com>
2024-02-06 00:39:40 -0500

    Show multi-line comments

--- src/main/java/com/maganadev/javacomments/JavaComments.java
+++ src/main/java/com/maganadev/javacomments/JavaComments.java
@@ -11,6 +11,13 @@ public class JavaComments {
         System.out.println("Welcome Java!"); //Welcome message for java
         System.out.println("You were born in 1995");
         //Java's release was announced by Sun Microsystems in 1995
+        System.out.println("You were created by James Gosling");
+        /*
+        James Gosling was a Canadian engineer who created java while working at 
+        Sun Microsystems
+        His favorite number was the square root of 2!
+        */
+        System.out.print("You will be a great language!");
         
     }
 }
